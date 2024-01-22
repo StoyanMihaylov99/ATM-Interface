d6b7ceced7e92e9aec4e960cc03820c09da1fcb3
StoyanMihaylov99 <stoqn.business@gmail.com>
2024-01-22 23:29:34 +0200

    Uploading password(pin) hashing config class

---
+++ src/main/java/org/example/config/PasswordHashing.java
@@ -0,0 +1,33 @@
+package org.example.config;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class PasswordHashing {
+    public static String hashPassword(String password) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] encodedHash = digest.digest(
+                    password.getBytes(StandardCharsets.UTF_8));
+
+            // Convert the byte array to a hexadecimal string
+            StringBuilder hexString = new StringBuilder(2 * encodedHash.length);
+            for (byte b : encodedHash) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static boolean verifyPassword(String enteredPassword, String storedHash) {
+        String enteredPasswordHash = hashPassword(enteredPassword);
+        return enteredPasswordHash.equals(storedHash);
+    }
+}
